8f97d7be0ff0158083bdce69817525b3480fe3ce
unknown <287907160@qq.com>
2016-03-25 15:34:06 +0800

    delete myView , add clickButton

--- aa/app/src/main/java/com/example/danny/aa/MainActivity.java
+++ aa/app/src/main/java/com/example/danny/aa/MainActivity.java
@@ -6,33 +6,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView ivPicture;  //操作的动画
+    private Button btClick;//点击开始
+
+    private int picWidth;//图片宽度
+    private int picHeight;//图片高度
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-       final ImageView imageView = (ImageView) findViewById(R.id.imageview);
-
-       ViewTreeObserver observer =  imageView.getViewTreeObserver();
+        ivPicture = (ImageView) findViewById(R.id.imageview);
+        ViewTreeObserver observer =  ivPicture.getViewTreeObserver();
         observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                Log.i("lxm", "layout....");
-                ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "rotation", 0, 30, -30, 15, -15, 5, -5, 0);
-                imageView.setPivotX(imageView.getWidth() / 2);
-                imageView.setPivotY(imageView.getHeight() / 5);
-                animator.setDuration(3000);
-                Ga
-                animator.start();
+                picWidth = ivPicture.getWidth();
+                picHeight = ivPicture.getHeight();
             }
         });
 
+        btClick = (Button) findViewById(R.id.click);
+        btClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObjectAnimator animator = ObjectAnimator.ofFloat(ivPicture, "rotation", 0, 30, -30, 15, -15, 5, -5, 0);
+                ivPicture.setPivotX(picWidth / 2);
+                ivPicture.setPivotY(picHeight / 5);
+                animator.setDuration(3000);
+                animator.start();
+            }
+        });
 
     }
 }
